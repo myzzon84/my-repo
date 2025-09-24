ec2e16bca0084a6e8fd4baa6373639096edbf8a4
myzzon84 <97201583+myzzon84@users.noreply.github.com>
2025-09-25 02:16:10 +0300

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,6 +1,6 @@
 export default async () => {
   // Робимо HTTP-запит до твого API
-  const res = await fetch("https://team-online-storage.onrender.com/api/v1/livez/");
+  const res = await fetch("https://team-online-storage.onrender.com/api/v1/livez/?js=1");
   const data = await res.text();
 
   console.log("Ping result:", data);
