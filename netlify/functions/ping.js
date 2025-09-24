6267c514d0aa55617625e657e6dc0cf7dc6bbdb6
myzzon84 <97201583+myzzon84@users.noreply.github.com>
2025-09-25 01:03:42 +0300

    Create ping.js

---
+++ netlify/functions/ping.js
@@ -0,0 +1,18 @@
+export default async () => {
+  // Робимо HTTP-запит до твого API
+  const res = await fetch("https://team-online-storage.onrender.com/api/v1/livez/");
+  const data = await res.text();
+
+  console.log("Ping result:", data);
+
+  return {
+    statusCode: 200,
+    body: "Ping executed",
+  };
+};
+
+// CRON-вираз: кожні 14 хв з 8:00 до 23:00 (UTC)
+// Для Києва (UTC+3) треба вказати 5-20 замість 8-23
+export const config = {
+  schedule: "*/10 5-19 * * *",
+};
